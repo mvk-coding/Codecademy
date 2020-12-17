b2be5ea534af0418e3609dbbd053a594ca5c4670
Manon <72911560+mvk-coding@users.noreply.github.com>
2020-12-18 00:58:52 +0100

    Create initials.cpp

---
+++ 1_hello_world/initials.cpp
@@ -0,0 +1,13 @@
+//Hi! I love cats :)
+#include <iostream>
+
+int main() {
+
+  std::cout << "M   M           K   K \n";
+  std::cout << "MM MM           K  K  \n";
+  std::cout << "MM MM   v   v   K K   \n";
+  std::cout << "M M M   v   v   KK    \n";
+  std::cout << "M   M   v   v   K K   \n";
+  std::cout << "M   M    v v    K  K  \n";
+  std::cout << "M   M     v     K   K \n";
+}
