7d6693b0be54e2cca4a668a23e1b7b625007f423
Manon <72911560+mvk-coding@users.noreply.github.com>
2020-12-18 19:23:52 +0100

    Create quadratic.cpp

---
+++ 2_variables/quadratic_formula/quadratic.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  
+ std::cout << "Let's solve an quadratic equation together! \n";
+ 
+  //declare the needed variables
+  double a, b, c, leftX, rightX;
+  
+  //to store a
+  std::cout << "Enter a: ";
+  std::cin >> a;
+
+  //to store b
+  std::cout << "Enter b: ";
+  std::cin >> b;
+ 
+  //to store c
+  std::cout << "Enter c: ";
+  std::cin >> c;
+ 
+  //calculating both results
+  leftX = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
+  rightX = (-b + std::sqrt(b*b - 4*a*c)) / (2*a);
+
+  //showing the result
+  std::cout << "The lower value for x is " << leftX <<"\n";
+  std::cout << "The higher value for x is " << rightX <<"\n";
+ }
