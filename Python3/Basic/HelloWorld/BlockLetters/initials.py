bb045094fcefa53fd0cf51f30b59545cc3a61341
Manon <72911560+mvk-coding@users.noreply.github.com>
2022-05-16 11:33:17 +0200

    Create initials.py

---
+++ Python3/Basic/HelloWorld/BlockLetters/initials.py
@@ -0,0 +1,10 @@
+# My name: Manon van Keulen
+# Fun Fact: I love cats!
+
+print("M   M           K   K ")
+print("MM MM           K  K  ")
+print("MM MM   v   v   K K   ")
+print("M M M   v   v   KK    ")
+print("M   M   v   v   K K   ")
+print("M   M    v v    K  K  ")
+print("M   M     v     K   K ")
